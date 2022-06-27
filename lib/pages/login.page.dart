cbffe70daf9b9743517021a7a92ee39dcd90bb22
ivan <ivanndosssilva9389@gmail.com>
2022-06-27 05:16:31 -0300

    icone logo e campo email add

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -9,7 +9,42 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.amber),
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 128,
+              height: 128,
+              child: Image.asset("assets/logo.png"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              // autofocus: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
