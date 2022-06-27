c761cb056a35557c5ab35f63013279107c73df7c
ivan <ivanndosssilva9389@gmail.com>
2022-06-27 04:49:05 -0300

    iniciando pagina de login

---
+++ lib/pages/login.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.amber),
+    );
+  }
+}
